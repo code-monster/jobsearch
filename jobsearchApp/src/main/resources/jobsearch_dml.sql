eaec31108b6b61b54cf0451730bf183a93356441
code-monster <ua-lex@yandex.ru>
2017-01-30 03:07:20 +0200

    fix location in dml script

--- jobsearchApp/src/main/resources/jobsearch_dml.sql
+++ jobsearchApp/src/main/resources/jobsearch_dml.sql
@@ -3,7 +3,7 @@ INSERT INTO vacancy (vacancy_name, company_name, creation_date, salary, location
 ***Prefer applicants that are eligible for hire in the U.S. without visa sponsorship.A minimum of 7 years of Java development experience with complex, large scale, n-tiered applications using the following technologies is required: Java/JEE, HTML, JavaScript, Oracle, SQL, JSP, JSTL.', 'https://www.work.ua/jobs/1362915/');
 INSERT INTO vacancy (vacancy_name, company_name, creation_date, salary, location, description, original_link) VALUES ('Middle Java Developer', 'XWZ', '2016-12-20', 'NULL', 'Kharkov', 'MUST have a Bachelor Degree in a business, computer science, information systems or related field ', 'https://www.work.ua/jobs/1362900/');
 INSERT INTO vacancy (vacancy_name, company_name, creation_date, salary, location, description, original_link) VALUES ('Java EE dev', '', '2016-12-20', '10000', 'Kiev', 'amiliarity with UNIX scripting, performance tuning, database queries, and running UNIX operating systems, databases and web technologies is required. ', 'https://www.work.ua/jobs/136444/');
-INSERT INTO vacancy (vacancy_name, company_name, creation_date, salary, location, description, original_link) VALUES ('Java', 'x4', '2016-12-20', '110000', 'удалённо', 'Must be able to obtain Position of Public Trust security clearance.', 'https://www.work.ua/jobs/888880/');
+INSERT INTO vacancy (vacancy_name, company_name, creation_date, salary, location, description, original_link) VALUES ('Java', 'x4', '2016-12-20', '110000', 'Kharkov', 'Must be able to obtain Position of Public Trust security clearance.', 'https://www.work.ua/jobs/888880/');
 
 
 INSERT INTO ROLE(ROLE_NAME) VALUES ('admin');
